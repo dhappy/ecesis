3010ce7340fde424d7e9ee8d1416ed3a0dafcea8
Will Holcomb <will@dhappy.org>
2017-02-11 09:42:50 -0500

    new query

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source 'https://rubygems.org'
 
-gem 'wikidata-client'
 gem 'pry'
--- test.rb
+++ test.rb
@@ -1,35 +1,24 @@
 #!/usr/bin/env ruby
 
 require 'pry'
-require 'wikidata'
 require 'net/http'
 require 'json'
 
-query = """SELECT DISTINCT ?item ?itemLabel ?awardLabel ?time
-{
-    ?item wdt:P106/wdt:P279* wd:Q3455803 ;
-          p:P166 ?awardStat .             
-    ?awardStat pq:P805 ?award ;           
-               ps:P166 wd:Q103360 .       
-    ?award wdt:P585 ?time .               
-    SERVICE wikibase:label {              
-        bd:serviceParam wikibase:language 'en'
-    }
+query = """
+SELECT DISTINCT ?item ?itemLabel ?authorLabel ?year WHERE {  
+  ?item wdt:P166 wd:Q255032.
+  ?item wdt:P50 ?author.  
+  ?item wdt:P577 ?year.
+  SERVICE wikibase:label { bd:serviceParam wikibase:language 'en' }    
 }
-ORDER BY DESC(?time)"""
+"""
 
 url = URI.parse("https://query.wikidata.org/sparql?format=json&query=#{query}")
 awards = JSON.parse(Net::HTTP.get(url), symbolize_names: true)
 
-binding.pry
-
-return
-
-hugos = Wikidata::Item.find_by_title 'Hugo Award'
-hugos.properties('P527').each do |award|
-  puts award.title
-  award.properties('P1346').each do |winner|
-    puts winner
-  end
+awards[:results][:bindings].each do |award|
+  year = award[:year][:value].match(/([^-]+)-/)[1]
+  puts ".../award/Hugo/year/#{year} -> .../book/by/#{award[:authorLabel][:value]}/#{award[:itemLabel][:value]}"
 end
 
+#binding.pry
